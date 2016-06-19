77f1bae511b33b6453b25aecc02d4b6094b915fa
Alex <none@none>
2016-06-19 15:59:17 +0600

    общий красс переводов теперь наследует переводы из пакета lang

--- translations/ru.php
+++ translations/ru.php
@@ -3,7 +3,11 @@
 	class adminPanelPkgRuTranslation extends langPkgRuTranslation {
 	
 		public function getTranslations() {
-			return array(
+			
+			
+			$parent_translations = parent::getTranslations();
+			
+			$translations = array(
 				'You should login as admin' => 'Вы должны войти под учетной записью администратора',
 				'You logged as %s' => 'Вы вошли как %s',
 				'Logout' => 'Выйти',
@@ -22,8 +26,14 @@
 				'Required field "%s" is empty' => 'Не заполнено обязательное поле "%s"',
 				'Field "%s" contains malformed value' => 'Поле "%s" содержит значение в неправильном формате'
 				
-				
 			);
+			
+			
+			$translations = array_merge($parent_translations, $translations);		
+			
+			return $translations;
+			
+			
 		}
 		
 		
